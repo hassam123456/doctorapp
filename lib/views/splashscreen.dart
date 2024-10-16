1fc29d13ff046a570903f43f2790347fb43440bd
APEX <apex@APEXs-MacBook-Air.local>
2024-10-17 04:53:50 +0500

    splash screen

---
+++ lib/views/splashscreen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Doctor App'),
+      //   backgroundColor: Colors.green,
+      // ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Text(
+              'Welcome to Doctor App 👋',
+              style: TextStyle(
+                fontSize: 17,
+                // fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 22),
+            Image.asset(
+              'assets/images/doctorsplashpic.png', // Replace with your image URL
+              height: 350,
+              width: double.infinity,
+              fit: BoxFit.fill,
+            ),
+            SizedBox(height: 20),
+            Wrap(
+              spacing: 10, // Horizontal spacing between the buttons
+              runSpacing: 10, // Vertical spacing between the rows
+              children: [
+                _buildLoginOption(Icons.person, 'Login as Admin', Colors.green),
+                _buildLoginOption(
+                    Icons.local_hospital, 'Login as Consultant', Colors.green),
+
+                // Align the "Login as User" button in the center
+                Align(
+                  alignment:
+                      Alignment.center, // Centers the button horizontally
+                  child: _buildLoginOption(
+                      Icons.people, 'Login as User', Colors.green),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                // Handle signup tap
+              },
+              child: Text(
+                "Don't have an account? Signup",
+                style: TextStyle(color: Colors.blue),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginOption(IconData icon, String text, Color color) {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+        side: BorderSide(color: color),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(icon, size: 40, color: color),
+            SizedBox(height: 10),
+            Text(
+              text,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
